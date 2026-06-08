31f82b3fd185cc1deb40724b9df71e79ba849f10
Abdur Rahman <abdurrahmansoftw@gmail.com>
2026-06-08 15:38:09 +0600

    Add while loop examples
    Replace the placeholder print with several while-loop examples demonstrating basic loop control: a simple counter, a while-else case, an infinite loop that exits with break, and a loop using continue to print odd numbers. These changes provide illustrative examples of Python while loop behavior for learning purposes.

--- 05_loops/02_while_loops/app.py
+++ 05_loops/02_while_loops/app.py
@@ -1,1 +1,30 @@
-print("Hello World")
+# while loops example:
+
+counter = 0
+while counter < 5:
+    print(counter)
+    counter += 1
+
+# while loops with else:
+counter = 0
+while counter < 5:
+    print(counter)
+    counter += 1
+else:
+    print("Counter is no longer less than 5.")
+
+# while loops with break:
+counter = 0
+while True:
+    print(counter)
+    counter += 1
+    if counter >= 5:
+        break
+
+# while loops with continue:
+counter = 0
+while counter < 10:
+    counter += 1
+    if counter % 2 == 0:
+        continue
+    print(counter)
