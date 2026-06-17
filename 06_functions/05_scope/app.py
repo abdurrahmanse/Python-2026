81ecd5dc4732ce55a6931572bede8b142bd0370d
Abdur Rahman <abdurrahmansoftw@gmail.com>
2026-06-17 17:43:01 +0600

    fucntions scope
    global scope

--- 06_functions/05_scope/app.py
+++ 06_functions/05_scope/app.py
@@ -1,1 +1,30 @@
-print("Hello World")
+# fucntions scope
+def outer():
+    x = 'local'
+    print(x)
+
+
+outer()
+
+# global scope
+y = 'global'
+
+
+def outer():
+    print(y)
+
+
+outer()
+
+# global scope
+y = 'global'
+
+
+def outer():
+    y = 'local'
+    print(y)
+
+
+outer()
+print(y)
+
