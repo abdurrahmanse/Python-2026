8c32a7f150833a0b14ba4d2a868d2e1f7b078cc6
Abdur Rahman <abdurrahmansoftw@gmail.com>
2026-06-20 15:00:36 +0600

    dictionaries
    access the value of a key

--- 07_data_structures/04_dictionaries/app.py
+++ 07_data_structures/04_dictionaries/app.py
@@ -1,1 +1,30 @@
-print("Hello World")
+# dictionaries example:
+
+student = {
+    "name": "Abdur Rahman",
+    "age": 20,
+    "major": "Computer Science"
+}
+
+# print the dictionary
+print(student)
+
+# access the value of a key
+print(student["name"])
+
+# add a new key-value pair to the dictionary
+student["gpa"] = 3.8
+print(student)
+
+
+# update the value of an existing key
+student["age"] = 21
+print(student)
+
+# remove a key-value pair from the dictionary
+del student["major"]
+print(student)
+
+
+# get the length of the dictionary
+print(len(student))
