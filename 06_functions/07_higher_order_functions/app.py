53b82b210ed33b63ac65483ef70af09b37685fb6
Abdur Rahman <abdurrahmansoftw@gmail.com>
2026-06-25 21:13:14 +0600

    Higher order fuctions
    Higher order fuctions

--- 06_functions/07_higher_order_functions/app.py
+++ 06_functions/07_higher_order_functions/app.py
@@ -1,1 +1,20 @@
-print("Hello World")
+# Higher order fuctions example:
+
+def apply_operation(x, y, operation):
+    return operation(x, y)
+
+# Example usage:
+
+
+def add(a, b):
+    return a + b
+
+
+def multiply(a, b):
+    return a * b
+
+
+result1 = apply_operation(5, 3, add)
+result2 = apply_operation(5, 3, multiply)
+print("Addition Result:", result1)  # Output: Addition Result: 8
+print("Multiplication Result:", result2)  # Output: Multiplication Result: 15
