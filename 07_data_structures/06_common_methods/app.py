ff03f762e9ac3d5d2816393689a22c1828c85d9c
Abdur Rahman <abdurrahmansoftw@gmail.com>
2026-06-29 12:31:57 +0600

    common methods
    common methods

--- 07_data_structures/06_common_methods/app.py
+++ 07_data_structures/06_common_methods/app.py
@@ -1,1 +1,22 @@
-print("Hello World")
+# common methods example:
+
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+
+
+# Append method
+my_list.append(6)
+print(my_list)
+
+# Insert method
+my_list.insert(2, 10)
+print(my_list)
+
+# Remove method
+my_list.remove(3)
+print(my_list)
+
+
+# Pop method
+my_list.pop()
+print(my_list)
