6e40d4e9cbfc59f60e2055946a195473dc533096
Abdur Rahman <abdurrahmansoftw@gmail.com>
2026-07-12 12:19:56 +0600

    Files Write
    Files Write example

--- 02_level_intermediate/04_file_handling/02_write_files/app.py
+++ 02_level_intermediate/04_file_handling/02_write_files/app.py
@@ -1,1 +1,6 @@
-print("Hello World")
+# Files Write example:
+
+with open("example.txt", "w") as f:
+    f.write("Hello, World!\n")
+    f.write("This is an example of writing to a file.\n")
+    f.write("We can write multiple lines to the file.\n")
